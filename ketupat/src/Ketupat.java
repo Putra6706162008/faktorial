dd61d7633fcaab7a8963581b81b9c897406a776d
Muhammad Yoga Nugraha Putra <37873825+Putra6706162008@users.noreply.github.com>
2018-03-29 09:29:13 +0700

    Update Ketupat.java

--- ketupat/src/Ketupat.java
+++ ketupat/src/Ketupat.java
@@ -1,23 +1,39 @@
 public class Ketupat {
     public static void main(String[] args) {
-        int size = 9;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter N : ");
+        int n=sc.nextInt();
+        System.out.print("Enter Symbol : ");
 
-        for (int i = 1; i < size; i += 2) {
-            for (int k = size; k >= i; k -= 2) {
-                System.out.print(" ");
+        char c = sc.next().charAt(0);
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+
+            {
+                System.out.print("0");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+
+            for(int j=1;j<=i*2-1;j++)
+
+            {
+                System.out.print(c);
             }
             System.out.println();
-        }// end loop
 
-        for (int i = 1; i <= size; i += 2) {
-            for (int k = 1; k <= i; k += 2) {
-                System.out.print(" ");
+        }
+        for(int i=n-1;i>0;i--)
+        {
+            for(int j=1;j<=n-i;j++)
+
+            {
+                System.out.print("0");
             }
-            for (int j = size; j >= i; j--) {
-                System.out.print("*");
+            for(int j=1;j<=i*2-1;j++)
+
+            {
+                System.out.print(c);
             }
             System.out.println();
         }
